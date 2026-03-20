1a03664919cb6432daa5f33a9a0a4051710c841e
qqq <www1@wwwqqqdeMac-mini.local>
2026-03-21 01:09:54 +0800

    Update GitHub credentials

--- install-from-github.sh
+++ install-from-github.sh
@@ -3,8 +3,8 @@
 # Proxy Manager GitHub 版本一键安装脚本
 # 请将下面的 GITHUB_USERNAME 和 REPO_NAME 替换为你的实际值
 
-GITHUB_USERNAME="your-username"
-REPO_NAME="your-repo-name"
+GITHUB_USERNAME="hnbwww"
+REPO_NAME="proxy-manager"
 SCRIPT_FILE="proxy-manager-install.sh"
 
 echo "正在从 GitHub 下载 Proxy Manager 安装脚本..."
